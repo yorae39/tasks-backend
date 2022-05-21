99734cceb1177847193e7de43da1b8755a6d5f8d
lpa0700 <luiz.aureliano@zoop.com.br>
2022-05-21 18:59:47 -0300

    Testando envio de email em build com erro

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -12,7 +12,7 @@ public class DateUtilsTest {
 	public void returnTrueForFutureDates() {
 		
 		LocalDate date = LocalDate.of(2030, 01, 01);
-		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
 		
 		Boolean validDate = DateUtils.isEqualOrFutureDate(date);		
 		Assert.assertEquals(validDate, true);
